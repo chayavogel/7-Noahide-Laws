af73cc3c55150db3ffd7ba7ceaf2c52a7adb0045
chaya <chayavogel1@gmail.com>
2023-10-30 19:31:27 -0400

    create resources page

---
+++ src/Pages/List.js
@@ -0,0 +1,9 @@
+function List() {
+    return (
+        <>
+        <h1>List Page</h1>
+        </>
+    )
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./Routes.js";
 import './index.css';
-import App from './App';
 
 const router = createBrowserRouter(routes);
 
